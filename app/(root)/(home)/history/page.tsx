dd413eadb6196e97716af6df3042c92b8d0ea23e
saurabhguptaiit27 <saurabhguptaiit27@gmail.com>
2024-06-08 05:16:29 +0530

    WIP-1

--- app/(root)/(home)/history/page.tsx
+++ app/(root)/(home)/history/page.tsx
@@ -3,7 +3,7 @@ import CallList from '@/components/CallList';
 const PreviousPage = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-    <h1 className="text-3xl font-bold text-green-500 hover:text-green-400 cursor-pointer">Previous Calls History</h1>
+    <h1 className="text-3xl font-bold text-green-500 hover:text-green-400 cursor-pointer">Calls History</h1>
 
       <CallList type="ended" />
     </section>
