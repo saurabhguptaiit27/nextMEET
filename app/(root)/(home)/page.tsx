4cfcad694cd3a10a1a52cf8dcaa6bf67947a643c
saurabhguptaiit27 <saurabhguptaiit27@gmail.com>
2024-06-08 06:45:40 +0530

    time updated

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,9 +3,10 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 const Home = () => {
   const now = new Date();
 
-  const time = now.toLocaleTimeString('en-US', {
+  const time = now.toLocaleTimeString('en-IN', {
     hour: '2-digit',
     minute: '2-digit',
+    timeZone: 'Asia/Kolkata',
   });
   const date = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(
     now,
